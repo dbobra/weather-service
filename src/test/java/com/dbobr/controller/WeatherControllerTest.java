a0a2a8d1c7459f9afbe688c29846b1bc16a895a9
Darshan <bobra.darshan@gmail.com>
2019-12-06 22:20:00 +0100

    Test case for jaspyt encryption

--- src/test/java/com/dbobr/controller/WeatherControllerTest.java
+++ src/test/java/com/dbobr/controller/WeatherControllerTest.java
@@ -1,7 +1,6 @@
 package com.dbobr.controller;
 
 import com.dbobr.service.WeatherService;
-import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
